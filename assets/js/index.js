a54a995c849e810dab4fc94473859fa4e2dd247f
AldoTu <aldoturegano10@hotmail.com>
2017-05-21 21:40:33 -0500

    Last changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,26 @@
 $(document).ready(function(){
 
-  var count = 0;
-
-  $("#footer-arrow").click(function(){
-    if (count == 0) {
-      $(".wrap").animate({top: -110}, 425);
-      $('.page-container').animate({bottom: 200}, 425);
-      //$("#footer-arrow").attr('transform', 'translate(0 0) rotate(180)');
-      //$("#footer-arrow").animate({svgTransform: 'translate(0 0) rotate(180)'});
-      count = 1;
-    }
-    else if (count == 1) {
-      $(".wrap").animate({top: 0}, 425);
-      $('.page-container').animate({bottom: 0}, 425);
-      //$("#footer-arrow").attr('transform', 'translate(0 0) rotate(180)');
-      //$("#footer-arrow").animate({svgTransform: 'translate(0 0) rotate(180)'});
-      count = 0;
-    }
-  });
+  initMap();
 
 });
+
+function initMap() {
+  var uluru = {lat: 19.518668, lng: -99.23249599999997};
+  var c = {lat: 19.518884463514354, lng: -99.2400561645627};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: c,
+    disableDefaultUI: true,
+    scrollwheel: false,
+    zoomControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    disableDoubleClickZoom: true
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
